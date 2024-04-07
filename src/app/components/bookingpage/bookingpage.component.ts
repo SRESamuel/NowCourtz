ff1dd41e124cf8922806116f2b678cb77dd9f65f
SRESamuel <samuel.est10@hotmail.com>
2024-04-07 17:33:03 -0400

    Updated master

---
+++ src/app/components/bookingpage/bookingpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-bookingpage',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './bookingpage.component.html',
+  styleUrl: './bookingpage.component.css'
+})
+export class BookingpageComponent {
+
+}
