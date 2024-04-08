8796e6864df71141d83e66016d685caf95872f1f
SRESamuel <samuel.est10@hotmail.com>
2024-04-08 19:43:26 -0400

    Added navBar & added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {NavheaderComponent} from "./components/navheader/navheader.component";
+import {LogotopComponent} from "./components/logotop/logotop.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, NavheaderComponent],
+  imports: [CommonModule, RouterOutlet, NavheaderComponent, LogotopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navheader/navheader.component.ts
+++ src/app/components/navheader/navheader.component.ts
@@ -4,13 +4,19 @@ import {RouterLink} from "@angular/router";
 @Component({
   selector: 'app-navheader',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink],
   templateUrl: './navheader.component.html',
   styleUrl: './navheader.component.css'
 })
 export class NavheaderComponent {
-  title: string ="NowCourtz";
-
+  menuVal:boolean=false;
+  menu_icon :string ='bi bi-list';
+  menuOpen(){
+    this.menuVal =! this.menuVal ;
+    this.menu_icon = this.menuVal ? 'bi bi-x' : 'bi bi-list';
+  }
+  menuClose() {
+    this.menuVal = false;
+    this.menu_icon = 'bi bi-list';
+  }
 }
