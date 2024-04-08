88c4a67f4691922cd52ca41282899917ab6984ab
BrittanyMRogers <brittanymjrogers@gmail.com>
2024-04-07 20:31:57 -0400

    Added Components: courtspage, errorpage, aboutpage, homepage

---
+++ src/app/components/courtspage/courtspage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courtspage',
+  standalone: true,
+  imports: [],
+  templateUrl: './courtspage.component.html',
+  styleUrl: './courtspage.component.css'
+})
+export class CourtspageComponent {
+
+}
