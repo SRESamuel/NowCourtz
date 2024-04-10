d991a08bb72275194745ecde47734431b018fb87
BrittanyMRogers <brittanymjrogers@gmail.com>
2024-04-09 20:40:12 -0400

    Continued adding to booking page, created Database, created DB CRUD functions for bookings, created viewallbookings component, continuing with booking/database/viewallbookings next.

--- src/app/components/bookingpage/bookingpage.component.ts
+++ src/app/components/bookingpage/bookingpage.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {Booking} from "../../models/booking.model";
+import {BookingDALService} from "../../../services/dal.service";
 import {JsonPipe} from "@angular/common";
-
 @Component({
   selector: 'app-bookingpage',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    JsonPipe
   ],
   templateUrl: './bookingpage.component.html',
   styleUrl: './bookingpage.component.css'
+
 })
 export class BookingpageComponent {
   title = "Book a Court";
-  fullName: string = "";
-  booking: Booking = new Booking("", "", "", "");
-  MIN_LENGTH = 5;
-
-
-  onBookClick() {
+  courtTypes: string[] = ["Basketball", "Dodgeball", "Volleyball", "Tennis", "Squash"]
+  booking: Booking = new Booking("", "", "", "", "","");
+  MIN_LENGTH = 2;
+  dal = inject(BookingDALService) //importing crud functions from dal
+  constructor() {
+    this.booking.courtType = this.courtTypes[0];
 
   }
+
+  onBookClick() {
+    this.dal.insert(this.booking).then((data) => {
+      console.log(data);
+      alert("Record added successfully");
+    }).catch(e => {
+      console.log("error " + e.message)
+    })
+
+  }
+
 }
---
+++ src/app/components/viewbookingspage/viewbookingspage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewbookingspage',
+  standalone: true,
+  imports: [],
+  templateUrl: './viewbookingspage.component.html',
+  styleUrl: './viewbookingspage.component.css'
+})
+export class ViewbookingspageComponent {
+
+}
