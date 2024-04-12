76ae10dd6ea4c0e98daae6fbb881d3f54fbf598e
SRESamuel <samuel.est10@hotmail.com>
2024-04-12 03:31:32 -0400

    Added geo service as well

---
+++ src/services/geo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeoService {
+
+  getCurrLocation () : Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+
+      const options = {
+        timeout: 1000,
+        maximumAge: 0,
+        enableHighAccuracy: true
+      };
+
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        });
+      },(e) => {
+        reject({
+          code: e.code,
+          message: e.message
+        });
+
+      },options)
+
+    });
+  }
+  constructor() { }
+}
